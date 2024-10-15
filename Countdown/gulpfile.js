d42f3e4f5a6d11db90102265163914de1552f1ce
Usercarlosdaniel <danielbarrosmendespvp@gmail.com>
2024-10-14 21:38:26 -0300

    countdown project

---
+++ Countdown/gulpfile.js
@@ -0,0 +1,14 @@
+const { watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+
+function Serve(cb) {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  watch(["*html.", "*.css", "*.js"]).on("change", browserSync.reload);
+  cb();
+}
+
+exports.default = Serve;
